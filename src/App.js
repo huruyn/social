f953fc54cb2b5efb2a90a59aa3d25fce45ef7112
huruyn <someone@huruynegash89@gmail.com>
2022-07-23 00:33:01 +0300

    some init msg

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+
+import { AuthContext } from "./context/AuthContext";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Messenger from "./pages/messenger/Messenger";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { useContext } from "react";
+
+function App() {
+  const { user } = useContext(AuthContext);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/messenger">
+          {!user ? <Redirect to="/" /> : <Messenger />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
